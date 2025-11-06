ac5e57be2dcdb3f866cc39399f154aab2e5a0c3c
Fuat Yazar <fyazar@gmail.com>
2025-11-06 05:34:41 -0800

    initial commit - 4

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Popup from "./Popup.tsx";
+
+function App() {
+
+  return (
+    <>
+      <Popup/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Popup.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Popup: React.FC = () => {
+    return (
+        <div className="p-4">
+            <h1 className="text-lg font-bold">Hello from Chrome Extension</h1>
+        </div>
+    );
+}
+
+export default Popup
